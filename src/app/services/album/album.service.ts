6a7e801a4f8356931db1e98658a39792c563b61a
MariaNegrych <negrychmariya@gmail.com>
2020-04-13 16:51:51 +0300

    hw-2-updated

---
+++ src/app/services/album/album.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AlbumModel} from '../../components/models/AlbumModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  constructor(private http: HttpClient) { }
+
+  getAlbums(): Observable<AlbumModel[]>{
+    return this.http.get<AlbumModel[]>(`https://jsonplaceholder.typicode.com/albums`)
+  }
+}
